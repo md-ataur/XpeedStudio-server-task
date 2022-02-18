568b7a05e9ad5184bee3e0e541b24797f9999d00
md-ataur <mdataur15@gmail.com>
2022-02-18 12:24:35 +0600

    file upload mechanism

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
+const upload = require('express-fileupload');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
  */
 app.use(cors());
 app.use(express.json());
+app.use(upload());
 
 /**
  * Database Connection
@@ -22,20 +24,30 @@ async function run() {
     try {
         await client.connect();
         const database = client.db("Card");
-        const records = database.collection("records");
+        const tasksCollection = database.collection("tasks");
+        // console.log('Successfully database connected');
 
-        console.log('Successfully database connected');
-
-        // Insert
+        // Data Insert
         app.post('/data', async (req, res) => {
-            const calculateData = req.body;
-            const result = await records.insertOne(calculateData);
-            res.send(result);
+            const calculateData = JSON.parse(req.body.data);
+            const file = req.files.file;
+
+            file.mv(`${__dirname}/uploads/${file.name}`, err => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send(err);
+                }
+                // res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+            });
+
+            calculateData.filePath = `/uploads/${file.name}`;
+            const result = await tasksCollection.insertOne(calculateData);
+            res.json(result);
         });
 
         // Get data
         app.get('/data', async (req, res) => {
-            const result = await records.find({}).toArray();
+            const result = await tasksCollection.find({}).toArray();
             res.json(result);
         });
     }
